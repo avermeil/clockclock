ecfb03fdaef8b2f7a02ab6655ea66f3d1a42b90d
avermeil <axel.vermeil@gmail.com>
2022-04-25 22:37:41 +0100

    pure wifi test

---
+++ wifitest/wifitest.ino
@@ -0,0 +1,173 @@
+/****************************************************************************************************************************
+  HelloServer.ino - Simple Arduino web server sample for SAMD21 running WiFiNINA shield
+  For any WiFi shields, such as WiFiNINA W101, W102, W13x, or custom, such as ESP8266/ESP32-AT, Ethernet, etc
+
+  WiFiWebServer is a library for the ESP32-based WiFi shields to run WebServer
+  Based on and modified from ESP8266 https://github.com/esp8266/Arduino/releases
+  Based on  and modified from Arduino WiFiNINA library https://www.arduino.cc/en/Reference/WiFiNINA
+  Built by Khoi Hoang https://github.com/khoih-prog/WiFiWebServer
+  Licensed under MIT license
+ ***************************************************************************************************************************************/
+
+#include "defines.h"
+
+int status = WL_IDLE_STATUS; // the Wifi radio's status
+int reqCount = 0;            // number of requests received
+
+WiFiWebServer server(80);
+
+#if defined(LED_BUILTIN)
+const int led = LED_BUILTIN;
+#else
+#if (ESP32)
+// Using pin 13 will crash ESP32_C3
+const int led = 2;
+#else
+const int led = 13;
+#endif
+#endif
+
+void handleRoot()
+{
+#define BUFFER_SIZE 512
+
+    digitalWrite(13, 1);
+    char temp[BUFFER_SIZE];
+    int sec = millis() / 1000;
+    int min = sec / 60;
+    int hr = min / 60;
+    int day = hr / 24;
+
+    snprintf(temp, BUFFER_SIZE - 1,
+             "<html>\
+<head>\
+<title>%s</title>\
+<style>\
+body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
+    </style>\
+</head>\
+<body>\
+<h1>Hello from %s</h1>\
+<h3>running WiFiWebServer</h3>\ 
+<h3>on %s</h3>\
+<p>Uptime: %d d %02d:%02d:%02d</p>\
+</body>\
+</html>",
+             BOARD_NAME, BOARD_NAME, SHIELD_TYPE, day, hr, min % 60, sec % 60);
+
+    server.send(200, F("text/html"), temp);
+    digitalWrite(13, 0);
+}
+
+void handleNotFound()
+{
+    digitalWrite(led, 1);
+
+    String message = F("File Not Found\n\n");
+
+    message += F("URI: ");
+    message += server.uri();
+    message += F("\nMethod: ");
+    message += (server.method() == HTTP_GET) ? F("GET") : F("POST");
+    message += F("\nArguments: ");
+    message += server.args();
+    message += F("\n");
+
+    for (uint8_t i = 0; i < server.args(); i++)
+    {
+        message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+    }
+
+    server.send(404, F("text/plain"), message);
+
+    digitalWrite(led, 0);
+}
+
+void setup()
+{
+    pinMode(13, OUTPUT);
+    // Open serial communications and wait for port to open:
+    Serial.begin(115200);
+    while (!Serial)
+        ;
+
+    Serial.print(F("\nStarting HelloServer on "));
+    Serial.print(BOARD_NAME);
+    Serial.print(F(" with "));
+    Serial.println(SHIELD_TYPE);
+    Serial.println(WIFI_WEBSERVER_VERSION);
+
+#if WIFI_USING_ESP_AT
+
+    // initialize serial for ESP module
+    EspSerial.begin(115200);
+    // initialize ESP module
+    WiFi.init(&EspSerial);
+
+    Serial.println(F("WiFi shield init done"));
+
+#endif
+
+#if !(ESP32 || ESP8266)
+
+// check for the presence of the shield
+#if USE_WIFI_NINA
+    if (WiFi.status() == WL_NO_MODULE)
+#else
+    if (WiFi.status() == WL_NO_SHIELD)
+#endif
+    {
+        Serial.println(F("WiFi shield not present"));
+        // don't continue
+        while (true)
+            ;
+    }
+
+#if USE_WIFI_NINA
+    String fv = WiFi.firmwareVersion();
+
+    if (fv < WIFI_FIRMWARE_LATEST_VERSION)
+    {
+        Serial.println(F("Please upgrade the firmware"));
+        Serial.println(fv);
+        Serial.println(WIFI_FIRMWARE_LATEST_VERSION);
+    }
+#endif
+
+#endif
+
+    Serial.print(F("Connecting to SSID: "));
+    Serial.println(ssid);
+
+    status = WiFi.begin(ssid, pass);
+
+    delay(1000);
+
+    // attempt to connect to WiFi network
+    while (status != WL_CONNECTED)
+    {
+        delay(500);
+
+        // Connect to WPA/WPA2 network
+        status = WiFi.status();
+    }
+
+    // server.begin();
+
+    server.on(F("/"), handleRoot);
+
+    server.on(F("/inline"), []()
+              { server.send(200, F("text/plain"), F("This works as well")); });
+
+    server.onNotFound(handleNotFound);
+
+    server.begin();
+
+    Serial.print(F("HTTP server started @ "));
+    Serial.println(WiFi.localIP());
+}
+
+void loop()
+{
+    server.handleClient();
+}
